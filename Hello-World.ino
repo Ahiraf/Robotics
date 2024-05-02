8d053d01407b5f1b0fc02fe976fbb876defff373
Fariha Rayhan Mim <168169066+Ahiraf@users.noreply.github.com>
2024-05-02 11:12:16 +0600

    Update Hello-World.ino

--- Hello-World.ino
+++ Hello-World.ino
@@ -1,1 +1,13 @@
+void setup()
+{
+  // Initializes serial communication at 9600 bits per second.
+  Serial.begin(9600);
 
+  // Prints "Hello World" to the serial monitor.
+  Serial.println("Hello World");
+}
+
+void loop()
+{
+  // The loop does nothing in this case.
+}
